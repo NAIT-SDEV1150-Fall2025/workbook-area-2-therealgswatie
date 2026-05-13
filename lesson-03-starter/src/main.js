6d52daf9fd492ed825a0153ba9740db65c0c3dc8
Suneel Saraswat <therealgswatie@gmail.com>
2026-05-12 18:54:26 -0600

    pretty much test commands added to main.js in lesson 3

--- lesson-03-starter/src/main.js
+++ lesson-03-starter/src/main.js
@@ -1,11 +1,33 @@
-console.log('Lesson 03 starter loaded');
-
-// Instructor TODO: 
-// 1. Declare variables using var, let, const
-// 2. Log their types with console.log(typeof …)
-// 3. Try built-in functions: alert(), prompt(), parseInt(), toString()
-// 4. Manipulate values and observe results in the console
-
+var n;
+console.log('Lesson 03 starter loaded');//semicolons indicate the end of the line.
+console.log('yada yada');
+console.log('something');
+console.log('what');
+const greeting = "Hello, World!";//constant string
+let count = 3.0;//let as float
+var name = true;//var as Boolean var is for functions
+console.log(n);
+console.log(x);
+n = 1;
+var x = 1
+console.log(n);
+console.log(x);
+//let and var can both be changed
+//variables defined with var exicute the following:
+//var n at top
+//n = 1 at line where var n = 1; is commited this is called hoisting.
+console.log(name,count,greeting);//comma gives a space, plus does same as python.
+console.log(typeof 2);
+//alert("Welcome to the demo");
+//const answer = prompt("name?");
+//console.log(answer);
+//const continueDemo = confirm(`Hi ${answer}, good to go?`);
+//console.log(`user chose to confirm: ${continueDemo}`);
+const strNumber = '42';
+const parsedNumber = parseInt(strNumber,10);
+console.log(`Type of strNumber: ${typeof strNumber}\nType of parsedNumber: ${typeof parsedNumber}`);
+const numberBackToString = parsedNumber.toString();
+console.log(`parsedNumber back to string: ${typeof numberBackToString}`);
 // Student TODO: 
 // Prompt the user for their name and age
 // Log a greeting message using the provided name and age
