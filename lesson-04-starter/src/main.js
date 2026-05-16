877347bad686ac8ded29fd10b1069816702e8524
Suneel Saraswat <therealgswatie@gmail.com>
2026-05-15 18:44:52 -0600

    added some code from class

--- lesson-04-starter/src/main.js
+++ lesson-04-starter/src/main.js
@@ -2,9 +2,43 @@ console.log('Lesson 04 starter loaded');
 
 // Instructor TODO:
 // 1. Simple if
+const x = 5;
+if (x > 0) {
+  console.log('x is +ve');
+}
 // 2. if-else
+// == and === are both comparison operators
+// === strictly checks for value with correct data type
+// '2' == 2 is true but '2' === 2 is false
+if (x % 2 === 0) {
+  console.log('x is even');
+} else {
+  console.log('x is odd');
+}
+/*
+const THIS_IS_A_CONSTANT //use screaming snake case for const
+*/
+
 // 3. Nested if-else
+if (x > 10) {
+  console.log('x is greater than 10');
+} else if (x < 10) {
+  console.log('x is less than 10');
+} else {
+  console.log('x is 10');
+}
 // 4. while loop
+let count = 3;
+while (count > 0) { // runs code while condition is true
+  console.log(`Countdown: ${count}`);
+  count--;
+}
+let counter = 10;
+while (counter !== 0) {
+  console.log(counter);
+  counter--;
+}
+
 // 5. do-while loop
 // 6. for loop
 
